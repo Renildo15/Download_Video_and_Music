2a6e134808793438ba1032159ec2d396dd55b58c
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-09-30 23:31:05 -0300

    Centralizando janela

--- main.py
+++ main.py
@@ -3,13 +3,26 @@ from funcoes import download_music, download_video
 
 #############################Interface#####################################
 
+##################Configurações da janela##################################
 janela = Tk()
-
-janela.geometry("750x520")
 janela.title("Software Download")
 janela.resizable(False, False)
 janela.iconbitmap("imgs/icon.ico")
 
+largura = 750
+altura = 520
+
+largura_janela = janela.winfo_screenwidth()
+altura_janela = janela.winfo_screenheight()
+
+posx = largura_janela/2 - largura/2
+posy = altura_janela/2 - altura/2
+
+janela.geometry("%dx%d+%d+%d" % (largura, altura, posx, posy))
+
+print(largura_janela, altura_janela)
+
+###########################Elementos da tela#################################
 label_link = Label(janela, text="Digite o link do vídeo:", font=("Courier 12 bold"))
 entry_link = Entry(janela, width=40)
 label_link.grid(row=0, column=6)
@@ -20,6 +33,7 @@ label_path = Label(janela, text="Caminho do download:", font=("Courier 12 bold")
 entry_path = Entry(janela, width=40)
 label_path.grid(row=2, column=6)
 entry_path.grid(row=3, column=3)
+
 
 
 #########botao###############
