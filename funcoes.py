d8b36fdc01796362299a7c1246818205d264e930
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-09-30 23:17:44 -0300

    Adicionando modulos

---
+++ funcoes.py
@@ -0,0 +1,28 @@
+from importlib.resources import path
+from pytube import YouTube
+from pytube import Playlist
+import moviepy.editor as mp
+import re
+import os
+####################### funções########################################
+def download_video():
+    link = input('Digite o link do vídeo: ')
+    path = input('Caminho de download: ')
+    yt = YouTube(link)
+    ys = yt.streams.get_highest_resolution().download(path)#Faz o download
+    print("Download Completo")
+
+def download_music():
+    link = input('Digite o link do video: ')
+    path = input('Caminho do download: ')
+    yt = YouTube(link)
+    ys = yt.streams.filter(only_audio=True).first().download(path)
+
+    for file in os.listdir(path):
+        if re.search('mp4', file):
+            mp4_path = os.path.join(path, file)
+            mp3_path = os.path.join(path, os.path.splitext(file)[0]+'.mp3')
+            new_file = mp.AudioFileClip(mp4_path)
+            new_file.write_audiofile(mp3_path)
+            os.remove(mp4_path)
+    print("Download Completo")
--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-from importlib.resources import path
-from pytube import YouTube
-from pytube import Playlist
 from tkinter import *
-import moviepy.editor as mp
-import re
-import os
+from funcoes import download_music, download_video
 
 #############################Interface#####################################
 
@@ -27,29 +22,13 @@ label_path.grid(row=2, column=6)
 entry_path.grid(row=3, column=3)
 
 
+#########botao###############
+def test(msg):
+    print(msg)
+botao = Button(janela, text="Baixar", command=lambda:test("Opa"))
+botao.grid(row=4, column=6)
+
+
+
 janela.mainloop()
 
-####################### funções########################################
-def download_video():
-    link = input('Digite o link do vídeo: ')
-    path = input('Caminho de download: ')
-    yt = YouTube(link)
-    ys = yt.streams.get_highest_resolution().download(path)#Faz o download
-    print("Download Completo")
-
-def download_music():
-    link = input('Digite o link do video: ')
-    path = input('Caminho do download: ')
-    yt = YouTube(link)
-    ys = yt.streams.filter(only_audio=True).first().download(path)
-
-    for file in os.listdir(path):
-        if re.search('mp4', file):
-            mp4_path = os.path.join(path, file)
-            mp3_path = os.path.join(path, os.path.splitext(file)[0]+'.mp3')
-            new_file = mp.AudioFileClip(mp4_path)
-            new_file.write_audiofile(mp3_path)
-            os.remove(mp4_path)
-    print("Download Completo")
-
-#download_video()
